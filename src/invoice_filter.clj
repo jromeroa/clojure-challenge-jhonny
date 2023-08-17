c74a8ad0fc87891866f2394184cecbefa77a8312
Jhonny Romero <jromeroarenales@gmail.com>
2023-08-16 19:16:44 -0500

    creating the first condition where :iva 19% and retention :ret_fuente 1%.

--- src/invoice_filter.clj
+++ src/invoice_filter.clj
@@ -1,6 +1,23 @@
-(def invoice (clojure.edn/read-string (slurp "invoice.edn")))
+(def invoice (clojure.edn/read-string (slurp "/Users/jhonny/projects/clojure-challenge-jhonny/invoice.edn")))
 
 (defn filter-invoice-items [items]
-  (filter (fn [item]) items))
+  (->> items
+       (filter
+         (fn [item]
+           (or
+             (and
+               (some
+                 (fn [tax]
+                   (and (= (:tax/category tax) :iva)
+                        (= (:tax/rate tax) 19)))
+                 (:taxable/taxes item))
+               (not-any?
+                 (fn [retention]
+                   (= (:retention/category retention) :ret_fuente))
+                 (:retentionable/retentions item)))
+             (and
+               )))
+         )))
 
 (def filtered-items (filter-invoice-items (:invoice/items invoice)))
+(println filtered-items)
